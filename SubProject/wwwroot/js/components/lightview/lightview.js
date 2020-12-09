298b57b26d7b54dbd0b1fbbec3c88215e7a6ebe8
Marcelmib <Amosiversen@gmail.com>
2020-12-09 13:04:39 +0100

    hide and show info button

--- SubProject/wwwroot/js/components/lightview/lightview.js
+++ SubProject/wwwroot/js/components/lightview/lightview.js
@@ -1,32 +1,46 @@
 ﻿define(['knockout', 'dataservice'], (ko, ds) => {
     return function (params) {
-
+        let buttontext = ko.observable("More Info");
         let rating = ko.observableArray([]);
         let movie = ko.observableArray([]);
         let OMDB = ko.observableArray([]);
+        let infocheck = ko.observable(true);
         const titleid = params.id || "";
-        console.log(params.id);
+        console.log("loaded light view");
         let generatelightview = title => {
-            console.log("trying to generate view with :");
-            console.log(titleid);
-            ds.getMovie(titleid, function (data) {
-                movie(data)
-                console.log(data);
-                //waits for movie to get data
-                ds.getOMDB(titleid, function (data) {
-                    OMDB(data)
+            if (infocheck()) {
+                console.log("trying to generate view with :");
+                console.log(titleid);
+                ds.getMovie(titleid, function (data) {
+                    movie(data)
                     console.log(data);
-                    ds.getRating(titleid, function (data) {
-                        rating(data)
+                    //waits for movie to get data
+                    ds.getOMDB(titleid, function (data) {
+                        OMDB(data)
                         console.log(data);
+                        ds.getRating(titleid, function (data) {
+                            rating(data)
+                            console.log(data);
+                        });
                     });
                 });
-            });
+                infocheck(false);
+                buttontext("hide info")
+                console.log(infocheck);
+            } else {
+                buttontext("more info")
+                rating("");
+                movie("");
+                OMDB("");
+                infocheck(true);
+            }
         }
 
-            
-            
+
+
+
         return {
+            buttontext,
             generatelightview,
             rating,
             movie,
